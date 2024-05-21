f96766d15720acaba583e14aa5a4a47caac81a0b
MANDRW <mandrzejewski132@gmail.com>
2024-05-21 23:43:54 +0200

    base virtual class

--- base.h
+++ base.h
@@ -1,4 +1,9 @@
 #ifndef STRUKTURYDANYCHPROJEKT3_BASE_H
 #define STRUKTURYDANYCHPROJEKT3_BASE_H
-
+class Base{//base class for all data structures with virtual methods
+public:
+    virtual void insert(int v,int k)=0;//v-value, k-key
+    virtual void remove(int k)=0;//k-key
+    virtual ~Base(){};
+};
 #endif
