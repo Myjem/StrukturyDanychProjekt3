b74e9bf8e06f362e1f0b4d323dc9b35e3395057f
MANDRW <mandrzejewski132@gmail.com>
2024-05-30 01:17:14 +0200

    data for testing

---
+++ key_and_value_generator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
+#include<cstdio>
+
+using namespace std;
+
+int main(){
+    fstream file;
+    srand(time(NULL));
+    file.open("key_value.txt",ios::out);
+    for(int i=0;i<200000;i++) {//quantity of pairs
+        int temp=(rand()%500000)+1;
+        file<<temp<<" "<<temp/100<<endl;
+    }
+    file.close();
+    cout<<"Number were generated";
+    return 0;
+}
