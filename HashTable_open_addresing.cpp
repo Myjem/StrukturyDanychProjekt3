49554fbdaa21790f588647f5e2ce076d43c7fdf4
Myjem <siepkawiktor240@gmail.com>
2024-06-05 14:08:27 +0200

    Added few comments

--- HashTable_open_addresing.cpp
+++ HashTable_open_addresing.cpp
@@ -11,13 +11,13 @@ HashTable_open::HashTable_open(int c):capacity(c),size(0)
     HashTable = new couple*[capacity];
     for(int i = 0; i < capacity; i++)
     {
-        HashTable[i] = nullptr;
+        HashTable[i] = nullptr;             //filling the entire list with nullpointers
     }
 }
 
 int HashTable_open::hash(int key) {
-    return key % capacity;
-}
+    return key % capacity;              //method for hashing
+}  
 
 void HashTable_open::insert(int key, int value) {
     if (size >= capacity) {
@@ -29,16 +29,16 @@ void HashTable_open::insert(int key, int value) {
 
     while (HashTable[index] != nullptr) {
         if (HashTable[index]->key == key) {
-            HashTable[index]->value = value;
+            HashTable[index]->value = value;        //check if this key exist
             return;
         }
-        index = (index + 1) % capacity;
+        index = (index + 1) % capacity;             //linear probing
         if (index == originalIndex) {
             cout << "Hash table is full" << endl;
             return;
         }
     }
-    HashTable[index] = new couple{key, value};
+    HashTable[index] = new couple{key, value}; 
     size++;
 }
 
@@ -50,7 +50,7 @@ void HashTable_open::remove(int key) {
          if (HashTable[index] != nullptr) {
             if (HashTable[index]->key == key) {
                 delete HashTable[index];
-                HashTable[index] = nullptr;
+                HashTable[index] = nullptr;             //deleting entire node
                 --size;
                 return;
             }
@@ -59,7 +59,7 @@ void HashTable_open::remove(int key) {
             return;
         }
 
-        index = (index + 1) % capacity;
+        index = (index + 1) % capacity;         //linear probing
     }
 }
 
